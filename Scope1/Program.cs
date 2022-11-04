a04d94fbffbe6de85ed5e34adc9a98ba665ddaab
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-04 08:53:32 +0300

    Add project files.

---
+++ Scope1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Basla();
+
+void Basla()
+{
+    int x = 100;
+    Console.WriteLine("x ilk değeri:" + x);
+    IkiyeKatla(x);
+    Console.WriteLine("x metotdan döndükten sonrası değeri:" + x);
+
+}
+
+void IkiyeKatla(int x)
+{
+    
+    x = x * 2;
+    Console.WriteLine("x metot içi değeri:" + x);
+}
